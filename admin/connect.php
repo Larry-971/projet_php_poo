0c7a9bca0834a5f5aff3a757d90c2b8a63d396cf
Larry-971 <42755932+Larry-971@users.noreply.github.com>
2019-03-07 22:21:35 +0100

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,7 +1,7 @@
 <?php
 //Connexion à ma base de donnée : projet_php
 try{
-    $connect = new PDO("mysql:host=localhost;dbname=projet_php", "Larry", "TLarry110196");
+    $connect = new PDO("mysql:host=localhost;dbname=projet_php", "", "");
     //echo"Connexion à la base : projet_php réussie...";
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
